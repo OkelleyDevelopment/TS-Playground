64421695c0eb02a2c5aff4226a87fe6e0c833790
OkelleyDevelopment <okelleynp@gmail.com>
2021-09-17 18:26:35 -0400

    feat: Add a small working example of Enums and Interfaces

--- pokemon/pokemon.js
+++ pokemon/pokemon.js
@@ -3,3 +3,13 @@ for (var _i = 0, pokemon_1 = pokemon; _i < pokemon_1.length; _i++) {
     var i = pokemon_1[_i];
     console.log(i);
 }
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 0] = "Red";
+    Color[Color["Green"] = 1] = "Green";
+    Color[Color["Blue"] = 2] = "Blue";
+})(Color || (Color = {}));
+;
+var c = Color.Red;
+var Bulbasaur = { name: "Bulbasaur", age: 5, attack: function () { console.log("Bulbasaur used tackle"); } };
+Bulbasaur.attack();
--- pokemon/pokemon.ts
+++ pokemon/pokemon.ts
@@ -3,3 +3,16 @@ let pokemon = ["Bulbasaur", "Charmander", "Squirtle"];
 for (const i of pokemon) {
     console.log(i);
 }
+
+enum Color { Red, Green, Blue };
+let c: Color = Color.Red;
+
+interface Pokemon {
+    name: string;
+    age?: number;
+    attack(): void;
+}
+
+let Bulbasaur: Pokemon  = {name: "Bulbasaur", age: 5, attack: () => {console.log("Bulbasaur used tackle")}};
+
+Bulbasaur.attack()
